6d3aa8d41692e1d2bf461a4c0ad26dbd3491c019
Jay Potter <j@ypotter.ca>
2024-03-31 14:22:34 -0400

    Create IRCClientTrait.php

---
+++ src/Potter/Socket/IRC/Client/IRCClientTrait.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Socket\IRC\Client;
+
+use \Psr\{Container\ContainerInterface, Link\LinkInterface};
+
+trait IRCClientTrait 
+{
+    final public function getLink(): LinkInterface
+    {
+        return $this->getContainer()->get('link');
+    }
+    
+    private function getLinkAttributes(): array
+    {
+        return $this->getLink()->getAttributes();
+    }
+    
+    final public function getPassword(): string
+    {
+        return $this->getLinkAttributes()['password'];
+    }
+    
+    final public function sendPassword(): void
+    {
+        $this->writeSocket('PASS ' . $this->getPassword());
+    }
+    
+    final public function getNickname(): string
+    {
+        return $this->getLinkAttributes()['nickname'];
+    }
+    
+    final public function sendNickname(): void
+    {
+        $this->writeSocket('NICK ' . $this->getNickname());
+    }
+    
+    final public function getUsername(): string
+    {
+        return $this->getLinkAttributes()['username'];
+    }
+    
+    final public function getRealName(): string
+    {
+        return $this->getLinkAttributes()['realname'];
+    }
+    
+    final public function getHostname(): string
+    {
+        return $this->getLinkAttributes()['hostname'];
+    }
+    
+    final public function getServerName(): string
+    {
+        return $this->getLinkAttributes()['server'];
+    }
+    
+    final public function sendUsername(): void
+    {
+        $this->writeSocket('USER ' . $this->getUsername() . ' ' . 
+                $this->getHostname() . ' ' . 
+                $this->getServerName() . ' '. 
+                $this->getRealName());
+    }
+    
+    abstract public function getContainer(): ContainerInterface;
+    abstract public function writeSocket(string $data): void;
+}
